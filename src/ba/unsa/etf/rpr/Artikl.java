86af4ffad3508f19dae6e110ceeb7009a2eadd7b
Amna Trcalo <atrcalo1@etf.unsa.ba>
2020-11-02 16:08:58 +0100

    Zavrsen zadatak

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv;
+    private int cijena;
+    private String kod;
+    public Artikl(String naziv, int cijena, String kod){
+        this.naziv=naziv;
+        this.cijena=cijena;
+        this.kod=kod;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(int cijena) {
+        this.cijena = cijena;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[1000];
+    private int brojac = 0;
+
+    public boolean dodajArtikl(Artikl a) {
+        if (brojac < 1000) {
+            artikli[brojac] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            brojac++;
+            return true;
+        }
+        return false;
+    }
+    public Artikl[] getArtikli(){
+        return artikli;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        for(int i=0; i<brojac; i++){
+            if(artikli[i].getKod().equals(kod)){
+                Artikl novi=new Artikl(artikli[i].getNaziv(), artikli[i].getCijena(), artikli[i].getKod());
+                artikli[i]=null;
+                pomjeriSve(i);
+                return novi;
+            }
+        }
+        return null;
+    }
+    private void pomjeriSve(int pozicija){
+        for(int i=pozicija; i<brojac; i++) artikli[i]=artikli[i+1];
+        artikli[brojac]=null;
+        brojac--;
+    }
+
 }
+
